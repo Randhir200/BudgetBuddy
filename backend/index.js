f62d2ebec6e64d57400ded800111a4c73bdaabc0
randhir200 <mail.randhirkr@gmail.com>
2024-08-14 23:14:17 +0530

    cors enabled in the server

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
