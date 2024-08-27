8852274533254282131c77a2ddcb765c742f746b
Randhir200 <mail.randhirkr@gmail.com>
2024-08-27 21:29:11 +0530

    login work has been started

--- frontend/src/pages/Expenses.tsx
+++ frontend/src/pages/Expenses.tsx
@@ -2,95 +2,20 @@ import React from "react";
 import {
   Box,
   Button,
-  Container,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-  Typography,
-  useTheme,
-  useMediaQuery,
 } from "@mui/material";
+import ExpensesForm from "../components/ExpensesForm";
 
 const Expenses: React.FC = () => {
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  // const theme = useTheme();
+  // const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
-    <Container maxWidth="sm" sx={{ mt: 4, mb: 4 }}>
-      <Typography variant="h4" gutterBottom>
-        Add Expense
-      </Typography>
-      <Box
-        component="form"
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: 3,
-          p: 3,
-          backgroundColor: theme.palette.background.paper,
-          borderRadius: 1,
-          boxShadow: theme.shadows[3],
-        }}
-      >
-        {/* Type Field */}
-        <FormControl fullWidth>
-          <InputLabel id="type-label">Type</InputLabel>
-          <Select
-            labelId="type-label"
-            id="type"
-            label="Type"
-            defaultValue=""
-          >
-            <MenuItem value="Needs">Needs</MenuItem>
-            <MenuItem value="Wants">Wants</MenuItem>
-            <MenuItem value="Savings">Savings</MenuItem>
-          </Select>
-        </FormControl>
-
-        {/* Category Field */}
-        <FormControl fullWidth>
-          <InputLabel id="category-label">Category</InputLabel>
-          <Select
-            labelId="category-label"
-            id="category"
-            label="Category"
-            defaultValue=""
-          >
-            <MenuItem value="Food">Food</MenuItem>
-            <MenuItem value="Bills">Bills</MenuItem>
-            <MenuItem value="Grocery">Grocery</MenuItem>
-          </Select>
-        </FormControl>
-
-        {/* Date Field */}
-        <TextField
-          id="date"
-          label="Date"
-          type="date"
-          defaultValue=""
-          InputLabelProps={{
-            shrink: true,
-          }}
-          fullWidth
-        />
-
-        {/* Price Field */}
-        <TextField
-          id="price"
-          label="Price"
-          type="number"
-          fullWidth
-          inputProps={{ min: 0 }}
-        />
-
-        {/* Submit Button */}
-        <Button variant="contained" color="primary" type="submit">
-          Add Expense
-        </Button>
-      </Box>
-    </Container>
+   <>
+   <Box>
+     <Button sx={{margin:"0px 0 0", float: "right", background: "primary"}}>Add </Button>
+     <ExpensesForm/>
+   </Box>
+   </>
   );
 };
 
