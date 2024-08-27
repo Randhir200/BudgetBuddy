4529633fc5fa112ecd9bf710fc1264b77a8ee587
Randhir200 <mail.randhirkr@gmail.com>
2024-08-27 23:10:07 +0530

    routinglayout has been change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,11 @@ import Config from "./pages/Config";
 import React, { useState, useMemo } from "react";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import Expenses  from "./pages/Expenses";
+import  Login  from "./pages/Login";
 
 const App: React.FC = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const [isLogedin, setIsLogedin] = useState(true);
 
   const theme = useMemo(
     () =>
@@ -33,13 +35,14 @@ const App: React.FC = () => {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-        <DashboardLayout onToggleDarkMode={toggleDarkMode} darkMode={darkMode}>
           <Routes>
+            <Route path="/login" element={<Login/>}/>
+            <Route element={<DashboardLayout onToggleDarkMode={toggleDarkMode} 
+            darkMode={darkMode}/>} />
             <Route path="/" element={<Dashboard />} />
             <Route path="/expenses" element={<Expenses />} />
             <Route path="/config" element={<Config />} />
           </Routes>
-        </DashboardLayout>
       </Router>
     </ThemeProvider>
   );
--- frontend/src/components/DashboardLayout.tsx
+++ frontend/src/components/DashboardLayout.tsx
@@ -2,39 +2,43 @@ import React, { useState } from "react";
 import TopBar from "./TopBar";
 import SideMenu from "./SideMenu";
 import MainContent from "./MainContent";
-// import { useTheme, useMediaQuery } from "@mui/material"
+import { useTheme, useMediaQuery, Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
 
 interface DashboardLayoutProps {
-  children: React.ReactNode;
   onToggleDarkMode: () => void;
   darkMode: boolean;
 }
 
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({
-  children,
   onToggleDarkMode,
   darkMode,
 }) => {
   const [menuOpen, setMenuOpen] = useState(false);
-  //   const theme = useTheme();
-  //   const isLargeScreen = useMediaQuery(theme.breakpoints.up("md"));
+  const theme = useTheme();
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up("md"));
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+    <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      {/* TopBar Component */}
       <TopBar
         onMenuClick={toggleMenu}
         onToggleDarkMode={onToggleDarkMode}
         darkMode={darkMode}
       />
-      <div style={{ display: "flex", flex: 1}}>
-        <SideMenu open={menuOpen} onClose={toggleMenu} />
-        <MainContent>{children}</MainContent>
-      </div>
-    </div>
+      <Box sx={{ display: "flex", flex: 1 }}>
+        {/* SideMenu Component */}
+        <SideMenu open={isLargeScreen ? true : menuOpen} onClose={toggleMenu} />
+        <MainContent>
+          {/* Outlet to render child routes */}
+          <Outlet />
+        </MainContent>
+      </Box>
+    </Box>
   );
 };
 
