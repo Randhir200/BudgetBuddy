3e60c2afc72b4c7af5f209bb72ef9c49caca128e
Randhir200 <mail.randhirkr@gmail.com>
2024-09-02 18:47:56 +0530

    healthapi has been created

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,38 @@
-const express = require('express');
-const expenseRoute = require('./routes/expenseRoute');
-require('dotenv').config();
-const cors = require('cors');
-const helmet = require('helmet');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require('mongoose');
+const cors = require("cors");
+const helmet = require("helmet");
+const connection = require("./db/connection");
 const app = express();
 
+// Use Helmet to set various HTTP headers for security
 app.use(helmet());
-app.use(cors())
-app.use(expenseRoute);
+app.use(cors());
 
-
-const Port = process.env.PORT || 3000;
-app.listen(Port, ()=>{
-    console.log(`Server is running on port ${Port}`);
+app.get("/health",async (req, res)=>{
+  try{
+    const mongoState = mongoose.STATES[mongoose.connection.readyState];
+    
+    if(mongoState === "connected"){
+      res.status(200).json({
+        status: "up", dbState: mongoState
+      })
+    }else{
+      res.status(500).json({ status: "down", dbState: mongoState });    
+    }
+  }catch(err){
+    res.status(500).json({ status: "failed", message: `something went wrong! ${err}` });    
+  }
 })
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
+  try {
+    await connection();
+    console.log(`Server is working in the port ${PORT}`);
+  } catch (err) {
+    console.log(`somethig went wrong! ${err}`);
+  }
+});
