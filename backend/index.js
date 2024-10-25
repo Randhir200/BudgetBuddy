44e528273a8299bc3d8675ed0b4768b851bfe34f
Randhir Kumar <92780081+Randhir200@users.noreply.github.com>
2024-10-25 11:24:00 +0000

    unhandledRejection and uncaughtException catching error handler added

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,15 @@ const masterRoute = require("./routes/masterRoute");
 const globalErrorHandler = require("./controllers/errorController");
 const AppError = require("./utils/appError");
 const { catchAsync } = require("./utils/catchAsync");
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+// uncaughtException for synchronus 
+process.on('uncaughtException', err=> {
+  console.log(err.name, err.message);
+  console.log('uncaughtException Rejection! 💥  Shutting down...')
+  process.exit(1)
+})
 const app = express();
 
 app.use(express.json());
@@ -22,7 +31,7 @@ app.use(masterRoute);
 
 //DB connection check
 app.get("/health", catchAsync(async (req, res) => {
-  const mongoState = mongoose.STATES[mongoose.connection.readyState];
+  const mongoState = mongoos.STATES[mongoose.connection.readyState];
   
   if (mongoState === "connected") {
     res.status(200).json({
@@ -32,7 +41,7 @@ app.get("/health", catchAsync(async (req, res) => {
     res.status(500).json({ status: "down", dbState: mongoState });
   }
   
-}))
+}));
 
 //Validation for unlisted routes
 app.all('*', (req, res, next) => {
@@ -42,11 +51,26 @@ app.all('*', (req, res, next) => {
 app.use(globalErrorHandler);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, async () => {
+const server = app.listen(PORT, async () => {
   try {
-    await connection();
-    console.log(`Server is working in the port ${PORT}`);
-  } catch (err) {
-    console.log(`somethig went wrong! ${err}`);
-  }
+  await connection();
+  console.log(`Server is working in the port ${PORT}`);
+} catch (err) {
+  console.log(`somethig went wrong! ${err}`);
+}
+ 
 });
+
+
+// Increase the limit to 20
+emitter.setMaxListeners(20);
+
+process.on('unhandledRejection', err=> {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection! 💥  Shutting down...')
+  // server.close giving the time to the server to complete all request still pending.
+  server.close(()=>{
+    process.exit(1)
+  })
+})
+
--- backend/utils/responseJson.js
+++ backend/utils/responseJson.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const statusMap = new Map([
     [200, 'success'],
     [201, 'created'],
@@ -8,8 +9,64 @@ const statusMap = new Map([
     [500, 'internalError']
 ]);
 
-exports.responseJson = (res, status, message, data=[])=>{
-    return res.status(status||500).json({
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+}
+
+const handleDuplicateFieldsDB = (err)=>{
+    const value = err.errmsg.match(/([" '])(\\?.)*?\1/)[0];
+    const message = `Duplicate field value: ${value}. Please use another value!`;
+    return new AppError(message, 400);
+}
+
+const handleValidationErrorDB = (err)=>{
+    const errors = Object.values(err.errors).map(el=>el.message)
+    const message = `Invalid input data. ${errors.join(', ')}`;
+    return new AppError(message, 400);
+}
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    })
+};
+
+const sendErrorProd = (err, res) => {
+    // Operational trusted error: send message to client
+    if (err.isOperational) {
+        return res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        })
+
+        //Programming or other unknown error: don't leak error details
+    } else {
+        // 1.) Log error
+        console.error(`ERROR 💥`, err);
+        return res.status(500).json({
+            status: 'error',
+            message: 'Somthing went wrong!',
+        })
+    }
+}
+
+
+exports.responseJson = (res, err = undefined, status, message, data = []) => {
+    if (err) {
+        if (process.env.NODE_ENV === 'development') {
+            return sendErrorDev(err, res);
+        } else if (process.env.NODE_ENV === 'production') {
+            let error = { ...err };
+            if (error.name === 'CastError') error = handleCastErrorDB(error);
+            if(error.code === 11000) error = handleDuplicateFieldsDB(error);
+            if(error.name === 'validationError') error = handleValidationErrorDB(error);
+            return sendErrorProd(error, res);
+        }
+    }
+    return res.status(status || 500).json({
         status: statusMap.get(status),
         message,
         data: `${status}`.startsWith('2') ? data : undefined,
