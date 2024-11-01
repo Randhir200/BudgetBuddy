85c9aaf262a81545f0697997afa3c0462d9a9e6a
Randhir200 <mail.randhirkr@gmail.com>
2024-11-01 10:49:04 +0530

    authentication prod url has been changed

--- frontend/src/configs/apiURLs.ts
+++ frontend/src/configs/apiURLs.ts
@@ -1,5 +1,5 @@
 const budgetBuddyApiUrl:string = `https://budgetbuddy-v0o5.onrender.com`;
 // const budgetBuddyApiUrl:string = `http://localhost:3000`;
-const authApiUrl:string = `https://auth-app-1-0.onrender.com`;
+const authApiUrl:string = `https://authentication-authorization-and-security.onrender.com/`;
 
 export {budgetBuddyApiUrl, authApiUrl};
