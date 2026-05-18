36c39f6af28a98c4a61788993dcb504564886003
Randhir200 <mail.randhirkr@gmail.com>
2026-05-18 17:26:11 +0530

    bug:replace local url

--- frontend/src/configs/apiURLs.ts
+++ frontend/src/configs/apiURLs.ts
@@ -1,4 +1,4 @@
-// const budgetBuddyApiUrl:string = `https://budgetbuddy-v0o5.onrender.com`;
-const budgetBuddyApiUrl:string = `http://localhost:3000`;
+const budgetBuddyApiUrl:string = `https://budgetbuddy-v0o5.onrender.com`;
+// const budgetBuddyApiUrl:string = `http://localhost:3000`;
 
 export {budgetBuddyApiUrl};
